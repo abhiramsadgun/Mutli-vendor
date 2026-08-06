3461d1e6674bc2773a319a01a079999b737faf8e
LATTALA ABHIRAM SADGUN <abhiramsadgun94@gmail.com>
2026-08-06 05:54:45 +0530

    Refactor user registration logic in authRoutes
    Refactor user registration route to improve readability and structure.

--- authRoutes.js
+++ authRoutes.js
@@ -3,19 +3,13 @@ const express=require("express");
 const bcrypt=require("bcryptjs");
 const jwt=require("jsonwebtoken");
 const User=require("../models/User");
-
 const router=express.Router();
-
 router.post("/register",async(req,res)=>{
-
 const {name,email,password}=req.body;
-
 const exists=await User.findOne({email});
-
 if(exists){
 return res.status(400).json({message:"User Exists"});
 }
-
 const hashed=await bcrypt.hash(password,10);
 
 await User.create({
